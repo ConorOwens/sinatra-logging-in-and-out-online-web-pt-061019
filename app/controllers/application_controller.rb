c261b59b81afdbf60ce946b193f6fb1ee76ceb77
Conor Owens <conorj.owens@gmail.com>
2019-09-28 22:19:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,10 @@ class ApplicationController < Sinatra::Base
 
   get '/account' do
     @user = User.find_by_id(session[:user_id])
-    erb :account
+    if session[:id]
+      erb :account
+    end
+    
   end
 
   get '/logout' do
